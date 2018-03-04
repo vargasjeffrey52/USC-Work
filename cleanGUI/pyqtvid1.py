cff36590e0a5feb93d6576497302f51f805da8e1
Jeffrey Vargas <vargas.jeffrey52@Berkeley.edu>
2018-03-03 21:09:11 -0800

    Intro: PyQt Object Oriented Programming sentdex: https://youtu.be/DF_44sWnAsk

--- cleanGUI/pyqtvid1.py
+++ cleanGUI/pyqtvid1.py
@@ -1,13 +1,16 @@
 import sys
 from PyQt4 import QtGui
 
+
+class Window(QtGui.QMainWindow):
+	def __init__(self):
+		super(Window,self).__init__()
+		self.setGeometry(50, 50, 500, 300)
+		self.setWindowTitle("PyQt tuts!")
+		self.setWindowIcon(QtGui.QIcon("pythonLogo.png"))
+		self.show()
+
 app = QtGui.QApplication(sys.argv)
- 
-window = QtGui.QWidget()
-window.setGeometry(50, 50, 500, 300)
-window.setWindowTitle("PyQT Tuts")
+GUI = Window()
 
-window.show()
-
-app.exec_()
- 
+sys.exit(app.exec_())
