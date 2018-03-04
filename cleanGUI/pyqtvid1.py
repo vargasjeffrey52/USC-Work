7effafb2b0275fcd1705f6fed6c76c31fa90847e
Jeffrey Vargas <vargas.jeffrey52@Berkeley.edu>
2018-03-04 12:54:46 -0800

    Intro: PyQt4 adding check box Sentdex: video8 https://youtu.be/mnYRRhKotbQ

--- cleanGUI/pyqtvid1.py
+++ cleanGUI/pyqtvid1.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt4 import QtGui, QtCore
 
+
 # creating a window object that inherit from QtGui
 class Window(QtGui.QMainWindow):
 	# the init is the initial set of methods that always run when window is called
@@ -39,20 +40,42 @@ class Window(QtGui.QMainWindow):
 
 		btn.resize(btn.minimumSizeHint())
 		btn.move(100,100)
-
+		img = QtGui.QIcon('pokeBall.png')
 		# Adding the action for a tools bar that belongs to the Home window
-		extractAction = QtGui.QAction(QtGui.QIcon('pokeBall.png'), 'Flee the seen', self)
+		extractAction = QtGui.QAction(img, 'Flee the seen', self)
 		extractAction.triggered.connect(self.close_application)
+
 
 		# displaying the tools menu that belongs to the homes window
 		self.toolBar = self.addToolBar('Extraction')
 		self.toolBar.addAction(extractAction)
 
+		checkBox = QtGui.QCheckBox('Enlarge Window', self)
+		checkBox.move(100, 25)
+		checkBox.toggle()
+		checkBox.stateChanged.connect(self.enlarge_window)
+
+
 		self.show()
 
+
+	def enlarge_window(self, state):
+		if state == QtCore.Qt.Checked:
+			self.setGeometry(50, 50, 1000, 600)
+		else:
+			self.setGeometry(50, 50, 500, 300)
+
 	def close_application(self):
-		print("whooaa so custom")
-		sys.exit()
+
+		choice = QtGui.QMessageBox.question(self,'Extract!', 
+			"Get into the Chopper",
+			QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+		if choice == QtGui.QMessageBox.Yes:
+			print("Extracting Now !!!")
+			sys.exit()
+		else:
+			pass
+
 
 def run():
 	app = QtGui.QApplication(sys.argv)
